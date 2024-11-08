cb506d6b87dfbffb963f5a636f6e66af913332dc
LeandroNitro <sampaioleo50@gmail.com>
2024-11-08 16:29:44 -0300

    Add files via upload

---
+++ python.py
@@ -0,0 +1,28 @@
+nome = str(input("difite o nome do heroi: "))
+
+xp = int(input("digite o valor do xp: "))   
+
+if xp <= 1000:
+    print("o heroi é rank ferro")
+
+elif xp <=2000:
+    print("o heroi é rank bronze")
+
+elif xp <=5000:
+    print("o heroi é rank prata")
+
+elif xp <=7000:
+    print("o heroi é rank ouro")
+
+elif xp <=8000:
+    print("o heroi é rank platina")
+
+elif xp <=9000:
+    print("o heroi é rank ascendente")
+
+elif xp <=10000:
+    print("o heroi é rank imortal")
+
+elif xp <=10001:
+    print("o heroi é rank radiante")
+
